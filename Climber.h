d415b05cf29cc2fde130ee9f0c783096828051e4
PearlL0700 <110639629+PearlL0700@users.noreply.github.com>
2025-12-07 00:03:33 -0600

    Added subsystem files
    Uploaded subsystem files that I worked on from 6800's team repo to my own repo

---
+++ Climber.h
@@ -0,0 +1,78 @@
+#pragma once
+
+#include "units/angular_velocity.h"
+#include "valkyrie/BaseSubsystem.h"
+#include "valkyrie/controllers/PhoenixController.h"
+#include "Constants.h"
+#include "valkyrie/controllers/PIDF.h"
+#include "valkyrie/sensors/CurrentSensor.h"
+#include "valkyrie/sensors/CANdleSensor.h"
+
+#include <frc/smartdashboard/SendableChooser.h>
+#include <frc/smartdashboard/SmartDashboard.h>
+
+#include "valkyrie/Gamepad.h"
+#include <frc/Alert.h>
+
+class Climber : public valor::BaseSubsystem
+{
+public:
+
+    Climber(frc::TimedRobot *robot, valor::CANdleSensor* leds);
+
+    ~Climber();
+
+    void init();
+    void resetState();
+    void assessInputs();
+    void analyzeDashboard();
+    void assignOutputs();
+
+    frc2::CommandPtr pitSequence();
+
+    void setDegrees(units::degree_t deg);
+
+    void InitSendable(wpi::SendableBuilder& builder);
+
+    enum CLIMB_STATE
+    {
+        MANUAL,
+        STOW,
+        DEPLOYED,
+        RETRACTED
+    };
+
+    enum STABBY_STATE
+    {
+        NO_CRAB,
+        CRABBING
+    };
+
+    struct x
+    {
+        CLIMB_STATE climbState;
+        // STABBY_STATE stabState;
+
+        bool hasClimbed;
+        bool lockOut;
+        double spikeCurrent;
+        double cacheSize;
+        units::volt_t manualSpeed;
+
+    } state;
+
+private:
+    frc2::CommandPtr pitSequenceStage(CLIMB_STATE);
+
+    // valor::PhoenixController<> *stabbyMotor;
+    valor::PhoenixController<> *climbMotors;
+    valor::CurrentSensor currentSensor;
+    ctre::phoenix6::hardware::CANcoder* climbCancoder;
+    valor::CANdleSensor *leds;
+
+    frc::Alert climberPosSuccess{"Climber position is within tolerance", frc::Alert::AlertType::kInfo};
+    frc::Alert climberPosFail{"Climber position not within tolerance", frc::Alert::AlertType::kError};
+    // frc::Alert stabbySpeedSuccess{"Stabby speed is within tolerance", frc::Alert::AlertType::kInfo};
+    // frc::Alert stabbySpeedFail{"Stabby speed is not within tolerance", frc::Alert::AlertType::kError};
+};
+
---
+++ Scorer.h
@@ -0,0 +1,122 @@
+# pragma once 
+#include "Drivetrain.h"
+#include "subsystems/Climber.h"
+#include "valkyrie/BaseSubsystem.h"
+#include "valkyrie/controllers/PhoenixController.h"
+#include "Constants.h"
+#include <vector>
+#include "valkyrie/sensors/GrappleSensor.h"
+#include "valkyrie/sensors/CANRangeSensor.h"
+#include "valkyrie/sensors/CurrentSensor.h"
+#include "valkyrie/controllers/PIDF.h"
+#include <frc/smartdashboard/SendableChooser.h>
+#include <frc/smartdashboard/SmartDashboard.h>
+#include <frc/trajectory/Trajectory.h>
+#include <frc/geometry/Pose2d.h>
+#include <frc/geometry/Rotation2d.h>
+#include <frc/trajectory/TrajectoryGenerator.h>
+#include <frc/DigitalInput.h>
+#include "valkyrie/sensors/DebounceSensor.h"
+#include <frc2/command/FunctionalCommand.h>
+#include <unordered_map>
+#include "valkyrie/Gamepad.h"
+#include <ctre/phoenix6/core/CoreCANdi.hpp>
+#include "valkyrie/sensors/CANdleSensor.h"
+#include <frc/Alert.h>
+#include <networktables/StructTopic.h>
+#include <frc2/command/FunctionalCommand.h>
+
+class Scorer : public valor::BaseSubsystem
+{
+public:
+
+    Scorer(frc::TimedRobot *robot, Drivetrain *drivetrain, Climber *climber, valor::CANdleSensor*);
+    
+    void resetState();
+     
+    void init();
+    void assessInputs();
+    void analyzeDashboard();
+    void assignOutputs();
+    void InitSendable(wpi::SendableBuilder& builder);
+
+    frc2::CommandPtr scorerPitSequence();
+
+    enum SCORE_STATE
+    {
+        HOLD,
+        INTAKING,
+        SCORING
+    };
+
+    enum SCOPED_STATE
+    {
+        UNSCOPED,
+        MANUAL_SCOPE,
+        SCOPED,
+    };
+    
+    void setScopedState(SCOPED_STATE);
+
+    struct x
+    {
+        SCORE_STATE scoringState;
+        Constants::Scorer::ELEVATOR_STATE elevState;
+        Constants::Scorer::GAME_PIECE gamePiece;
+        SCOPED_STATE scopedState;
+
+        units::meter_t targetHeight;
+        units::volt_t manualSpeed;
+
+        bool hasAlgae;
+        bool hasCoral;
+        bool intaking;
+        double algaeSpikeCurrent;
+
+        bool hasZeroed;
+        bool autoDunkEnabled;
+        bool shootOverCoral;
+    } state;
+
+private:
+    frc2::CommandPtr scorerPitSequenceStage(Constants::Scorer::GAME_PIECE, Constants::Scorer::ELEVATOR_STATE, int, int);
+    
+    units::meter_t convertToMechSpace(units::turn_t turns);
+    units::turn_t convertToMotorSpace(units::meter_t meters);
+
+    bool hallEffectSensorActive();
+    bool cancoderSensorBad();
+    units::turn_t absSensorCorrect;
+
+    valor::DebounceSensor hallEffectDebounceSensor;
+
+    ctre::phoenix6::hardware::core::CoreCANdi candi;
+
+    valor::PhoenixController<> *elevatorMotor;
+    valor::PhoenixController<> *scorerMotor;
+    valor::PhoenixController<> *scorerPivotMotor;
+    valor::PhoenixController<> *funnelMotor;
+
+
+    valor::CANrangeSensor scorerStagingSensor;
+    valor::CurrentSensor currentSensor;
+    valor::CurrentSensor coralCurrentSensor;
+
+    Constants::Scorer::PositionMap positionMap;
+    Constants::Scorer::ScoringSpeedMap scoringSpeedMap;
+
+    Drivetrain *drivetrain;
+    Climber *climber;
+    valor::CANdleSensor *leds;
+
+    frc::Alert elevatorStage{"Elevator going to stage", frc::Alert::AlertType::kInfo};
+    frc::Alert elevatorPositionSuccess{"Elevator position within tolerance", frc::Alert::AlertType::kInfo};
+    frc::Alert elevatorPositionFail{"Elevator position outside tolerance", frc::Alert::AlertType::kError};
+    frc::Alert elevatorBButtonWait{"Elevator waiting for driver B button...", frc::Alert::AlertType::kInfo};
+    
+    nt::StructPublisher<frc::Pose3d> visualizerStage1;
+    nt::StructPublisher<frc::Pose3d> visualizerStage2;
+    nt::StructPublisher<frc::Pose3d> visualizerStage3;
+
+    bool elevatorWithinThreshold;
+};
